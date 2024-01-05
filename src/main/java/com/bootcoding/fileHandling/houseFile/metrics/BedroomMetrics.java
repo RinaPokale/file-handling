3055691372bf62881ff80cbe7c90cf0e77f094c4
Rina Pokale <rinapokale009@gmail.com>
2024-01-05 09:48:48 +0530

    file handling - House File

---
+++ src/main/java/com/bootcoding/fileHandling/houseFile/metrics/BedroomMetrics.java
@@ -0,0 +1,28 @@
+package com.bootcoding.fileHandling.houseFile.metrics;
+
+import com.bootcoding.fileHandling.houseFile.model.House;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class BedroomMetrics implements  MetricCollector{
+
+    //Requirements?
+    /*
+        2 BHK --> list of house
+        3 BHK --> list of house
+        4 BHK --> list of house
+        5 BHK --> list of house
+     */
+    @Override
+    public void collect(List<House> data) {
+        //bedrooms size = 5
+        Map<Integer, List<House>> bedrooms = data.stream().collect(Collectors.groupingBy(House::getBedrooms));
+
+        bedrooms.forEach((k,v) -> System.out.println(k + " BHK: " + v.size()));
+
+//      Note:- groupingBy() create sub-group based on the specific criteria
+
+    }
+}
