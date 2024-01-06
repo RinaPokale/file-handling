8405b7e6d47a06e71fe694c965fb13bde0885e74
Rina Pokale <rinapokale009@gmail.com>
2024-01-06 16:36:46 +0530

    file handling - Bank File

--- src/main/java/com/bootcoding/fileHandling/bankFile/metrics/TransactionByLocations.java
+++ src/main/java/com/bootcoding/fileHandling/bankFile/metrics/TransactionByLocations.java
@@ -3,17 +3,7 @@ package com.bootcoding.fileHandling.bankFile.metrics;
 import com.bootcoding.fileHandling.bankFile.model.BankTransaction;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
-public class TransactionByLocations implements MetricsCollector{
-
-    @Override
-    public void collect(List<BankTransaction> transactions) {
-        Map<String, List<BankTransaction>> groupOfCity = transactions.stream().collect(Collectors.groupingBy(BankTransaction::getLocation));
-
-        groupOfCity.forEach((city, records) -> {
-            System.out.println(city + " location " + records.size() + " size");
-        });
-    }
+public interface TransactionByLocations {
+    public void collect(List<BankTransaction> transactions, String city);
 }
---
+++ src/main/java/com/bootcoding/fileHandling/bankFile/metrics/TransactionByLocationsImpl.java
@@ -0,0 +1,25 @@
+package com.bootcoding.fileHandling.bankFile.metrics;
+
+import com.bootcoding.fileHandling.bankFile.model.BankTransaction;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class TransactionByLocationsImpl implements TransactionByLocations{
+    @Override
+    public void collect(List<BankTransaction> transactions, String city) {
+         Map<String, List<BankTransaction>> groupOfCity = transactions.stream().
+                 collect(Collectors.groupingBy(BankTransaction::getLocation));
+
+        groupOfCity.forEach((c, records) -> System.out.println(c.equalsIgnoreCase(city)? c + " " + records.size(): ""));
+
+        /*
+            groupOfCity.forEach((c, records) -> {
+             if(c.equalsIgnoreCase(city)){
+                 System.out.println(c + " " + records.size());
+             }
+         });
+         */
+    }
+}
