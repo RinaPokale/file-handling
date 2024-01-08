b0fac1dc4b7db6437c745cb6824de12b02465e2d
Rina Pokale <rinapokale009@gmail.com>
2024-01-08 21:57:44 +0530

    file handling - Bank File

---
+++ src/main/java/com/bootcoding/fileHandling/houseFile/metrics/NeighbourhoodMetrics.java
@@ -0,0 +1,27 @@
+package com.bootcoding.fileHandling.houseFile.metrics;
+
+import com.bootcoding.fileHandling.houseFile.model.House;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+//what is requirements?
+// Houses -> Neighbours - Rural - count
+// Houses -> Neighbours - Suburb - count
+// Houses -> Neighbours - Urban - count
+
+public class NeighbourhoodMetrics implements MetricCollector{
+    @Override
+    public void collect(List<House> data) {
+        Map<String, List<House>> neighborData = data.stream().collect(Collectors.groupingBy(House::getNeighborhood));
+
+        neighborData.forEach((neighbor, records) -> System.out.println(neighbor + " " + records.size()));
+    }
+
+    public long getNeighbourhoodCount(List<House> data, String condition){
+        return data.stream()
+                .filter(row -> condition.equalsIgnoreCase(row.getNeighborhood()))
+                .count();
+    }
+}
