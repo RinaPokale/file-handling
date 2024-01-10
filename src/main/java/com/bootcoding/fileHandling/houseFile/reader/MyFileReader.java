d9a96458179f17df578b16492c4f507c70510cca
Rina Pokale <rinapokale009@gmail.com>
2024-01-10 17:33:50 +0530

    file handling - House File

---
+++ src/main/java/com/bootcoding/fileHandling/houseFile/reader/MyFileReader.java
@@ -0,0 +1,7 @@
+package com.bootcoding.fileHandling.houseFile.reader;
+
+//for any format file.. for ex., jason, csv, etc
+public interface MyFileReader extends DataReader{
+    //Marker Interface
+    //since it is empty
+}
